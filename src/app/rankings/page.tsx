fb3246260138e98f8a9946efa5e4b80437c9a2fb
phazejeff <85849384+phazejeff@users.noreply.github.com>
2025-03-16 21:21:01 -0700

    wrapped the whole damn thing in suspense

--- src/app/rankings/page.tsx
+++ src/app/rankings/page.tsx
@@ -3,13 +3,17 @@ import { Suspense } from "react";
 import SeasonRankings from "../SeasonRankings";
 import { useSearchParams } from "next/navigation";
 
-export default function RankingsPage() {
+const Rankings = () => {
   const searchParams = useSearchParams()
   const division = searchParams.get('division') ?? "1";
   const group = searchParams.get('group') ?? "1";
+  return <SeasonRankings division={division} group={group}/>;
+}
+
+export default function RankingsPage() {
   return (
     <Suspense>
-      <SeasonRankings division={division} group={group}/>
+      <Rankings />
     </Suspense>
   );
 }
